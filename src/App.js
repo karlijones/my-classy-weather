71e3b97b66862bdef86213dfc064b7f950895d40
karlijones <karlijones1984@gmail.com>
2025-01-12 13:41:09 -0600

    Get Weather button added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
       <div>
         <input type="text" placeholder="Search from location..." />
       </div>
+      <button>Get Weather</button>
     </div>
   }
 }
