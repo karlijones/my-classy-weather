b2ef9a9dc90fc4c0e9a04387c8ab89eb39d8efc1
karlijones <karlijones1984@gmail.com>
2025-01-15 15:37:57 -0600

    Changing console.logs to setState

--- src/App.js
+++ src/App.js
@@ -36,7 +36,12 @@ function formatDay(dateStr) {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { location: "lisbon", isLoading: false };
+    this.state = { 
+      location: "lisbon", 
+      isLoading: false, 
+      displayLocation: "", 
+      weather: {},
+    };
     this.fetchWeather = this.fetchWeather.bind(this);
   }
 
@@ -55,15 +60,18 @@ class App extends React.Component {
       if (!geoData.results) throw new Error("Location not found");
   
       const { latitude, longitude, timezone, name, country_code } =
-        geoData.results.at(0);
-      console.log(`${name} ${convertToFlag(country_code)}`);
+        geoData.results.at(0);  
+      
+      this.setState({displayLocation: `${name} ${convertToFlag
+          (country_code)}`, 
+      });
   
       // 2) Getting actual weather
       const weatherRes = await fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
       );
       const weatherData = await weatherRes.json();
-      console.log(weatherData.daily);
+      this.setState({ weather: weatherData.daily});
     } catch (err) {
       console.err(err);
     } finally {
