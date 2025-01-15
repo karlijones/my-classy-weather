b955f89bc7cec49d5374320c981ae89c2de7d7e6
karlijones <karlijones1984@gmail.com>
2025-01-15 15:27:22 -0600

    Loading message for fetching weather data

--- src/App.js
+++ src/App.js
@@ -32,31 +32,6 @@ function formatDay(dateStr) {
   }).format(new Date(dateStr));
 }
 
-async function getWeather(location) {
-  try {
-    // 1) Getting location (geocoding)
-    const geoRes = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-    );
-    const geoData = await geoRes.json();
-    console.log(geoData);
-
-    if (!geoData.results) throw new Error("Location not found");
-
-    const { latitude, longitude, timezone, name, country_code } =
-      geoData.results.at(0);
-    console.log(`${name} ${convertToFlag(country_code)}`);
-
-    // 2) Getting actual weather
-    const weatherRes = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
-    );
-    const weatherData = await weatherRes.json();
-    console.log(weatherData.daily);
-  } catch (err) {
-    console.err(err);
-  }
-}
 
 class App extends React.Component {
   constructor(props) {
@@ -65,9 +40,30 @@ class App extends React.Component {
     this.fetchWeather = this.fetchWeather.bind(this);
   }
 
-  fetchWeather() {
-    console.log("Loading data...");
-    console.log(this);
+  async fetchWeather() {
+    try {
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+  
+      if (!geoData.results) throw new Error("Location not found");
+  
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+      console.log(`${name} ${convertToFlag(country_code)}`);
+  
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      console.log(weatherData.daily);
+    } catch (err) {
+      console.err(err);
+    }
   }
 
   render() {
