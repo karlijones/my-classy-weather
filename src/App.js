8534a78456d89b7afeb0ec0ba1cad9dd7048076e
karlijones <karlijones1984@gmail.com>
2025-01-15 15:07:47 -0600

    adding data from starter.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { location: "lisbon"};
+    this.fetchWeather = this.fetchWeather.bind(this);
   }
 
   fetchWeather() {
